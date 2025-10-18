53ab9e40aa16515b68d0d4647b783e72ce90d496
arwrity <aarma4882@gmail.com>
2025-10-19 02:39:45 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://127.0.0.1:8081";
+export const API_BASE = "https://hunterx-1u94.onrender.com";
 
 export async function generateImage(prompt) {
   const res = await fetch(`${API_BASE}/generate_image`, {
